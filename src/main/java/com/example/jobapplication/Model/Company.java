7cd6274b0a205dc96f47365e499ab36c14e14a16
AndiZahiri <zahiriandii@gmail.com>
2025-02-19 14:40:24 +0100

    Add Jobs and Company classes , Connect to postgres database , and modify the app properties

--- src/main/java/com/example/jobapplication/Model/Company.java
+++ src/main/java/com/example/jobapplication/Model/Company.java
@@ -1,5 +1,6 @@
 package com.example.jobapplication.Model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,9 +12,13 @@ public class Company
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JsonProperty("name")
     private String name;
+    @JsonProperty("description")
     private String description;
+    @JsonProperty("contactEmail")
     private String contactEmail;
+    @JsonProperty("contactPhone")
     private String contactPhone;
 
     public Company(Long id, String name, String description, String contactEmail, String contactPhone) {
--- src/main/java/com/example/jobapplication/Model/Jobs.java
+++ src/main/java/com/example/jobapplication/Model/Jobs.java
@@ -1,5 +1,8 @@
 package com.example.jobapplication.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
 @Entity
@@ -8,22 +11,29 @@ public class Jobs
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JsonProperty("title")
     private String title;
+    @JsonProperty("type")
     private String type;
+    @Column(length = 500)
+    @JsonProperty("description")
     private String description;
+    @JsonProperty("location")
     private String location;
+    @JsonProperty("salary")
     private String salary;
-    @OneToOne
+    @ManyToOne
+    @JsonManagedReference
     private Company company;
 
-    public Jobs(Long id, String title, String type, String description, String location, String salary, Company company) {
+    public Jobs(Long id, String title, String type, String description, String location, String salary,Company company) {
         this.id = id;
         this.title = title;
         this.type = type;
         this.description = description;
         this.location = location;
         this.salary = salary;
-        this.company = company;
+        this.company=company;
     }
     public Jobs()
     {}
